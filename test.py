726f2793788413e69d64ac61566e04e4dc52895b
long-b1809701 <longb1809701@student.ctu.edu.vn>
2021-11-15 15:29:16 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+from typing import Text
+import playsound
+import speech_recognition as sr
+import wikipedia
+import pyttsx3
+from gtts import gTTS
+from typing import Text
+
+
+
+def speak(text):
+    print("Bot: {}".format(text))
+    tts = gTTS(text=text, slow=False)
+    tts.save("sound.mp3")
+    playsound.playsound("sound.mp3", False)
+    os.remove("sound.mp3")
+speak("hello")
